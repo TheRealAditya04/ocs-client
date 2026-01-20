58104db2f7c394eec1f984669f0e94650cffd368
TheRealAditya04 <adityabansal281206@gmail.com>
2026-01-20 23:32:32 +0530

    Fix API base URL for production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: "https://ocs-backend-adityas-projects-7949948c.vercel.app/api"
 });
 
 api.interceptors.request.use(config => {
