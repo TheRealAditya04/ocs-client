567c6f17fdca1b4b02a2894728c7b56ac3765911
TheRealAditya04 <adityabansal281206@gmail.com>
2026-01-21 23:05:08 +0530

    changed link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ocs-backend-git-main-adityas-projects-7949948c.vercel.app/api"
+  baseURL: "https://ocs-backend.vercel.app/"
 });
 
 api.interceptors.request.use(config => {
